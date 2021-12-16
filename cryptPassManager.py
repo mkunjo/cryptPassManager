62c7e0969de06a9b7c712e1c71bb3dc318d69b4e
Muhammad Kunjo <mkunjo121@gmail.com>
2021-12-16 12:16:47 -0500

    Added view function

--- cryptPassManager.py
+++ cryptPassManager.py
@@ -2,25 +2,30 @@
 masterPass = input("Please enter the master password:")
 
 #Create functions 
-def view():
-    pass
-
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd)
+        f.write(name + "|" + pwd + "\n")
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data  = line.rstrip()
+            user, passwd = data.split("|")
+            print("User:", user, ", Password: ", passwd)
+
+
 
 
 while True:
-    appMode = input("Please enter 1 or 2. \nWould you like to add a new password(1) or view existing ones(2)? \nEnter 0 to quit.\n")
+    appMode = input("Would you like to add a new password(1) or view existing ones(2)? \nPlease enter 1 or 2. Enter 0 to quit.\n")
     if appMode == "0":
         break
     if appMode == "1":
         add()
     elif appMode == "2":
-        pass
+        view()
     else:
         print("Invalid selection.")
         continue

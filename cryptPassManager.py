8dfc9e0a9907ba5ae5d11e7bdf306def4a57fabe
Muhammad Kunjo <mkunjo121@gmail.com>
2021-12-18 17:57:07 -0500

    Add and view function working with encryption

--- cryptPassManager.py
+++ cryptPassManager.py
@@ -1,21 +1,46 @@
-#Initial pass to gain access
-masterPass = input("Please enter the master password:")
+from cryptography.fernet import Fernet
 
-#Create functions 
+'''Only need this function once. Multiple keys would be troublesome
+def createKey():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as keyFile:
+        keyFile.write(key)
+
+Create key using function above        
+createKey()'''
+
+#Load Fernet key
+def loadKey():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+#Initial password and key to gain access
+masterPass = input("Please enter the master password:")
+#Convert password to bytes(encode function instead of .bytes)
+#  as the key is in byte format, then concatenate to key
+key = loadKey() + masterPass.encode()
+#Create variable for fernet w key for quick encoding/decoding
+fer = Fernet(key)
+
+#Create  main functions 
+#---------------------------------------------------------
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data  = line.rstrip()
             user, passwd = data.split("|")
-            print("User:", user, ", Password: ", passwd)
-
-
+            print("User:", user, "| Password: ",
+                fer.decrypt(passwd.encode()).decode())
 
 
 while True:
